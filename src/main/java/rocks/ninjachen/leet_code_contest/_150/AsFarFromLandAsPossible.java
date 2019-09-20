97252cc0e4f26a846b8951a7727b202a532bfb9a
Ninja <jiachenning@gmail.com>
2019-09-20 22:38:18 +0800

    Finish my first bfs implementation of AsFarFromLandAsPossible

--- src/main/java/rocks/ninjachen/leet_code_contest/_150/AsFarFromLandAsPossible.java
+++ src/main/java/rocks/ninjachen/leet_code_contest/_150/AsFarFromLandAsPossible.java
@@ -1,6 +1,6 @@
 package rocks.ninjachen.leet_code_contest._150;
 
-import java.util.BitSet;
+import java.util.*;
 
 /**
  * https://leetcode.com/contest/weekly-contest-150/problems/as-far-from-land-as-possible/
@@ -15,77 +15,64 @@ import java.util.BitSet;
 @SuppressWarnings("Duplicates")
 public class AsFarFromLandAsPossible {
     public int maxDistance(int[][] grid) {
-        boolean allWater = true;
-        boolean allLand = true;
+        Set<Integer> waterSet = new HashSet<>();
+        Set<Integer> landSet = new HashSet<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
+                int index = i * grid[0].length + j;
                 if (grid[i][j] == 1) {
-                    allWater = false;
+                    landSet.add(index);
                 } else if (grid[i][j] == 0) {
-                    allLand = false;
+                    waterSet.add(index);
                 }
             }
         }
-        if (allWater || allLand) {
+        if (waterSet.isEmpty() || landSet.isEmpty()) {
             return -1;
         }
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    dfs(grid, i, j, 0, new BitSet());
-                } else if (grid[i][j] == 0) {
+        // Mark every water cell with distance 1
+        for (int distance = 1; !waterSet.isEmpty(); distance++) {
+            for (int landIndex : landSet) {
+                int i = landIndex / grid[0].length;
+                int j = landIndex % grid[0].length;
+                for (int z = 0; z <= distance; z++) {
+                    setDistance(i + z, j + (distance - z), grid, waterSet, distance);
+                    setDistance(i + z, j - (distance - z), grid, waterSet, distance);
+                    setDistance(i - z, j + (distance - z), grid, waterSet, distance);
+                    setDistance(i - z, j - (distance - z), grid, waterSet, distance);
                 }
             }
+
         }
         int distanceMax = -1;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] != 1) {
                     int distance = grid[i][j];
-                    if (distance == -1) {
-                        distance = 1;
-                    }
-                    distanceMax = Math.max(distanceMax, distance);
+                    distanceMax = Math.max(distanceMax, distance -1);
                 }
             }
         }
         return distanceMax;
     }
 
-    private void dfs(int[][] grid, int i, int j, int distance, BitSet visitedLandBitSet) {
-        // End when out of bound, or water cell has a smaller distance.
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+    /**
+     * The distance write to the original matrix is 1-based. Eg 2 on the matrix is distance 1;
+     * @param i
+     * @param j
+     * @param grid
+     * @param waterSet
+     * @param distance
+     */
+    private void setDistance(int i, int j, int[][] grid, Set<Integer> waterSet, int distance) {
+        int index = i * grid[0].length + j;
+        // Out of bounds
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
             return;
         }
-        int index = i * grid.length + j;
-        // Skip visited
-        if(visitedLandBitSet.get(index)){
-            return;
-        }
-        if (grid[i][j] == -1 || (grid[i][j] > 1 && grid[i][j] < distance)) {
-            return;
-        }
-        // Land cell, create new scanner
-        if (grid[i][j] == 1) {
-            distance = 0;
-            visitedLandBitSet.set(index);
-            dfs(grid, i, j + 1, distance + 1, visitedLandBitSet);
-            dfs(grid, i, j - 1, distance + 1, visitedLandBitSet);
-            dfs(grid, i + 1, j, distance + 1, visitedLandBitSet);
-            dfs(grid, i - 1, j, distance + 1, visitedLandBitSet);
-        }
-        // Water cell has a smaller distance, update value.
-        if (grid[i][j] == 0 || (grid[i][j] > 1 && grid[i][j] > distance)) {
-            // -1 is water's distance 1;
-            if (distance == 1) {
-                grid[i][j] = -1;
-            } else {
-                grid[i][j] = distance;
-            }
-            dfs(grid, i, j + 1, distance + 1, visitedLandBitSet);
-            dfs(grid, i, j - 1, distance + 1, visitedLandBitSet);
-            dfs(grid, i + 1, j, distance + 1, visitedLandBitSet);
-            dfs(grid, i - 1, j, distance + 1, visitedLandBitSet);
+        if(grid[i][j] == 0 && waterSet.contains(index)){
+            grid[i][j] = distance + 1;
+            waterSet.remove(index);
         }
     }
 }
